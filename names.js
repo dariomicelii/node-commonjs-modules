cd4857bf6bd80cab90b3f456946140fb0d847f08
dariomicelii <dariomiceli01@gmail.com>
2024-11-18 18:18:22 +0100

    names attuale

--- names.js
+++ names.js
@@ -1,13 +1,8 @@
-firstName = "Dario";
-lastName = "Miceli";
-
 function namesObject(firstName, lastName) {
   let names = {
     firstName,
     lastName,
   };
-  console.log(names);
   return names;
 }
-namesObject(firstName, lastName);
 module.exports = namesObject;
