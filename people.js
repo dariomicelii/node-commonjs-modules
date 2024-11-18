4cf2905f8cb58dfa8456e65bf123106970ebd80f
dariomicelii <dariomiceli01@gmail.com>
2024-11-18 17:48:18 +0100

    people attuale

--- people.js
+++ people.js
@@ -3,8 +3,7 @@ const hobbiesObject = require("./hobbies");
 
 let yourProfile = () => {
   let profile = {
-    firstName: namesObject(firstName),
-    lastName: namesObject(lastName),
+    firstName: namesObject(firstName, lastName),
     hobbies: hobbiesObject(hobby1, hobby2, hobby3),
   };
   return console.log(profile);
